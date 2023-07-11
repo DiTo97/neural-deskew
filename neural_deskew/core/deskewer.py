fe29e861747c685fb4f0f66295b1cb662fca8b0d
f.minutoli <f.minutoli@reply.it>
2023-07-11 06:47:45 +0200

    Update base Deskewer with __call__

--- neural_deskew/core/deskewer.py
+++ neural_deskew/core/deskewer.py
@@ -2,21 +2,29 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 import numpy as np
+import numpy.typing as np_typing
 from PIL import Image
 
 import neural_deskew
+from neural_deskew.core.space import angle_space
 from neural_deskew.core.transforms import decode, encode
 
 
 class Deskewer(ABC):
     """A base class for document image deskew"""
 
-    def __init__(self, *args: Any, **kwargs: dict[str, Any]) -> None:
-        pass
+    def __init__(self, num_angles: int, *args: Any, **kwargs: dict[str, Any]) -> None:
+        self.angle_space = angle_space(num_angles)
 
     @abstractmethod
-    def detect_angle(self, array: neural_deskew.Array) -> float:
-        pass
+    def __call__(self, array: neural_deskew.Color) -> np_typing.NDArray[np.float32]:
+        """It computes skew angle probas over the angle space"""
+
+    def detect_angle(self, array: neural_deskew.Color) -> float:
+        probas = self(array)
+        idx = np.argmax(probas)
+
+        return self.angle_space[idx]
 
     def deskew(
         self,

0afd2a72ac56fd0e29787a26a3c9a8a303a80e9a
f.minutoli <f.minutoli@reply.it>
2023-07-11 08:36:14 +0200

    Add neg-pos sampling in angle space

--- neural_deskew/core/space.py
+++ neural_deskew/core/space.py
@@ -7,8 +7,13 @@ import numpy.typing as np_typing
 @lru_cache(maxsize=5)
 def angle_space(num_angles: int) -> np_typing.NDArray[np.float32]:
     """A linear angle space with constant step size in rad"""
-    space = np.linspace(
-        -np.pi / 2, np.pi / 2, num_angles
-    )  # step size = 180 / num angles (deg)
+    assert num_angles % 2 == 0, "The angle space must be symmetric"
+
+    tot_split = int(num_angles / 2)
+
+    neg_space = np.linspace(-np.pi / 2, 0, tot_split, endpoint=False)
+    pos_space = np.linspace(0, +np.pi / 2, tot_split)
+
+    space = np.hstack((neg_space, pos_space))  # It ensures 0 is in the space
 
     return space

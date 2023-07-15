a1ab3e3acf1d37a3f966b841a8b717ebfbf9c40d
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2023-07-15 18:05:20 +0200

    Add vspace in functional.py

--- neural_deskew/deskew/nn/functional.py
+++ neural_deskew/deskew/nn/functional.py
@@ -8,7 +8,8 @@ def pred_interval_to_point_estimate(output: torch.Tensor) -> torch.Tensor:
 
     point_estimate = v * U + (1 - v) * L
     return point_estimate
-        
+
+
 def pred_interval_to_confidence(output: torch.Tensor, coverage: float = 0.95) -> torch.Tensor:
     U = output[:, 0]
     L = output[:, 1]

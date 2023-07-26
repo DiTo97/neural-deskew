1240e522093abc93cac36eb75b690f7fe075fbe5
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2023-07-26 04:34:18 +0200

    Update functional.py

--- neural_deskew/deskew/nn/functional.py
+++ neural_deskew/deskew/nn/functional.py
@@ -1,18 +1,18 @@
 import torch
 
 
-def pred_interval_to_point_estimate(output: torch.Tensor) -> torch.Tensor:
-    U = output[:, 0]
-    L = output[:, 1]
-    v = output[:, 2]
+def pred_interval_to_point_estimate(outputs: torch.Tensor) -> torch.Tensor:
+    U = outputs[:, 0]
+    L = outputs[:, 1]
+    v = outputs[:, 2]
 
     point_estimate = v * U + (1 - v) * L
     return point_estimate
 
 
-def pred_interval_to_confidence(output: torch.Tensor, coverage: float = 0.95) -> torch.Tensor:
-    U = output[:, 0]
-    L = output[:, 1]
+def pred_interval_to_confidence(outputs: torch.Tensor, coverage: float = 0.95) -> torch.Tensor:
+    U = outputs[:, 0]
+    L = outputs[:, 1]
     
     distance = U - L
 

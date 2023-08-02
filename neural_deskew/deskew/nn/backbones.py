fb24e297e57df06772509165bc0482335c646526
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2023-08-02 16:52:57 +0200

    Add image feature extractor in backbones.py

--- neural_deskew/deskew/nn/backbones.py
+++ neural_deskew/deskew/nn/backbones.py
@@ -1,6 +1,76 @@
 import torch
 import torch.nn as nn
+import torchvision
 from torch.nn import functional as F
+
+
+class Imagecoder(nn.Module):
+    """An image feature extractor with feature pyramid network (FPN)"""
+
+    # TODO: MobileNet v3 in the args
+    def __init__(
+        self, 
+        hidden_dim: int, 
+        multiscale: bool,
+        dropout: float = 0.25, 
+        trainable: bool = False
+    ) -> None:
+        super().__init__()
+
+        #
+        from torchvision.models import mobilenet
+        from torchvision.models._api import _get_enum_from_fn as get_enum_from_definition
+        from torchvision.models.detection.backbone_utils import mobilenet_backbone as MobileNet_v3
+        #
+        
+        name = "mobilenet_v3_large"
+        definition = mobilenet.__dict__[name]
+
+        config = "default".upper()
+        checkpoint = get_enum_from_definition(definition)[config]
+        
+        kwargs = {
+            "backbone_name": name, "weights": checkpoint, "fpn": multiscale
+        }
+
+        if not trainable:
+            kwargs["trainable_layers"] = 0
+        
+        self.model = MobileNet_v3(**kwargs)
+        self.multiscale = multiscale
+
+        self.avgpool2d = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(dropout)
+        self.project = nn.Linear(256, hidden_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.model(x)
+
+        if self.multiscale:
+            vals = [val for key, val in x.items() if key != "pool"]
+
+            x, vals = vals[0], vals[1:]
+
+            # It fuses FPN maps
+            for val in vals:
+                x *= val
+
+        x = self.avgpool2d(x)
+        x = x.squeeze((2, 3))
+        x = self.dropout(x)
+        
+        x = self.project(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        return x
+
+
+class Anglecoder(nn.Module):
+    """An angle feature extractor"""
+
+    def __init__(self, hidden_dim: int) -> None:
+        pass
 
 
 class Encoder(nn.Module):

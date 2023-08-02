126fd8c0bd56d808e19f3cf586d010001293ac04
f.minutoli <f.minutoli@reply.it>
2023-08-02 21:31:53 +0200

    Add DISE 2021 preprocessing

---
+++ toolchain/preprocessing/dise2021.py
@@ -0,0 +1,35 @@
+"""A script for pre-processing the DISE 2021 dataset"""
+import pathlib
+
+import gdown
+import pandas as pd
+
+
+_artifact = "dise2021_45.zip"
+_URL = "https://drive.google.com/uc?id=1a-a6aOqdsghjeHGLnCLsDs7NoJIus-Pw"
+
+
+def preprocess(output_dir: str) -> None:
+    output_dir = pathlib.Path(output_dir).resolve()
+
+    artifact_dir = output_dir / _artifact.split(".")[0]
+    artifact = output_dir / _artifact
+
+    gdown.cached_download(_URL, artifact, postprocess=gdown.extractall)
+
+    metadata = []
+
+    for split_dir in artifact_dir.iterdir():
+        for image in split_dir.iterdir():
+            imagename = image.stem
+
+            angle = imagename.split("[")[-1]
+            angle = angle.split("]")[0]
+            angle = float(angle)
+
+            metadata.append((image, angle))
+
+    metadata_artifact = artifact_dir / "metadata.csv"
+
+    metadata = pd.DataFrame.from_records(metadata, columns=["image", "angle"])
+    metadata.to_csv(metadata_artifact, index=False)

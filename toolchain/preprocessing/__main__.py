5d51f6a51488a4844b608d89e014feb7ab252abd
f.minutoli <f.minutoli@reply.it>
2023-08-04 01:01:07 +0200

    Replace preprocessing main with main v2 + W&B metadata

--- toolchain/preprocessing/__main__.py
+++ toolchain/preprocessing/__main__.py
@@ -3,8 +3,12 @@ import argparse
 import os
 import pathlib
 import random
+import sys
+import tempfile
+import typing
 from typing import Any
 
+import albumentations
 import numpy as np
 import pandas as pd
 from PIL import Image
@@ -12,13 +16,89 @@ from skimage.transform import rotate
 from tqdm.auto import tqdm
 
 import wandb
+from neural_deskew.core.space import angle_space
+
+
+if typing.TYPE_CHECKING:
+    import types
+
+
+Transform = object
+
+
+def srcfile_import(modpath: str, modname: str) -> "types.ModuleType":
+    """It imports a Python module from its srcfile
+
+    Parameters
+    ----------
+    modpath
+        The srcfile absolute path
+    modname
+        The module name in the scope
+
+    Returns
+    -------
+        The imported module
+
+    Raises
+    ------
+    ImportError
+        If the module cannot be imported from the srcfile
+    """
+    import importlib.util
+
+    #
+    spec = importlib.util.spec_from_file_location(modname, modpath)
+
+    if spec is None:
+        message = f"No spec for module at {modpath}"
+        raise ImportError(message)
+
+    module = importlib.util.module_from_spec(spec)
+
+    # It adds the module to the global scope
+    sys.modules[modname] = module
+
+    if spec.loader is None:
+        message = f"No spec loader for module at {modpath}"
+        raise ImportError(message)
+
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def image_transform_from_config(config: str | None) -> Transform | None:
+    """A serialized image augmentation pipeline from config file"""
+    if config is None:
+        return None
+
+    transform_config = pathlib.Path(config).resolve()
+
+    format_ = transform_config.suffix
+    format_ = format_.replace(".", "")
+
+    transform_config = str(transform_config)
+
+    if not os.path.exists(transform_config):
+        message = f"No augmentation pipeline config file at {transform_config}"
+        raise ValueError(message)
+
+    transform = albumentations.load(transform_config, format_)
+
+    return transform
 
 
 def parse_args() -> dict[str, Any]:
     parser = argparse.ArgumentParser(__name__)
 
     parser.add_argument(
-        "--data-dir", help="The local dataset dir", type=str, required=True
+        "--datasets",
+        help="The document image skew estimation datasets",
+        type=str,
+        required=True,
+        nargs="+",
+        choices=["dise2021", "rdocuments"],
     )
 
     parser.add_argument(
@@ -26,6 +106,34 @@ def parse_args() -> dict[str, Any]:
     )
 
     parser.add_argument("--artifact", help="The name of the W&B artifact", type=str)
+
+    parser.add_argument(
+        "--stop-angle", help="The stop angle in degrees", type=int, default=360
+    )
+
+    parser.add_argument(
+        "--step-angle", help="The number of steps per angle", type=int, default=4
+    )
+
+    parser.add_argument(
+        "--image-transform-config",
+        help="The image augmentation pipeline config file",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--num-image-transforms",
+        help="The number of image transfoms per skew angle per image",
+        type=int,
+        default=3,
+    )
+
+    parser.add_argument(
+        "--num-angle-transforms",
+        help="The number of skew angle transforms per image",
+        type=int,
+        default=35,
+    )
 
     parser.add_argument(
         "--train-split", help="The train split", type=float, default=0.6
@@ -38,24 +146,28 @@ def parse_args() -> dict[str, Any]:
     parser.add_argument("--test-split", help="The test split", type=float, default=0.2)
     parser.add_argument("--seed", help="The random seed", type=int, default=10)
 
-    args = parser.parse_args()
+    args, _ = parser.parse_known_args()
     args = vars(args)
 
     return args
 
 
 def main(
-    data_dir: str,
+    datasets: list[str],
     artifact_dir: str,
-    artifact: str | None = None,
-    train_split: float = 0.6,
-    valid_split: float = 0.2,
-    test_split: float = 0.2,
-    seed: int = 10,
+    artifact: str | None,
+    stop_angle: int,
+    step_angle: int,
+    image_transform_config: str | None,
+    num_image_transforms: int,
+    num_angle_transforms: int,
+    train_split: float,
+    valid_split: float,
+    test_split: float,
+    seed: int,
 ):
-    if not os.path.exists(data_dir):
-        message = f"No local dataset dir: {data_dir}"
-        raise ValueError(message)
+    random.seed(seed)
+    np.random.seed(seed)
 
     cumsplit = train_split + valid_split + test_split
 
@@ -67,6 +179,8 @@ def main(
 
         raise ValueError(message)
 
+    image_transform = image_transform_from_config(image_transform_config)
+
     if not os.path.exists(artifact_dir):
         os.mkdir(artifact_dir)
 
@@ -76,58 +190,103 @@ def main(
     os.mkdir(images_dir)
     os.mkdir(annotations_dir)
 
+    angle_transform = angle_space(stop_angle, step_angle)
+
     examples = []
 
-    for split in tqdm(os.listdir(data_dir)):
-        split_dir = os.path.join(data_dir, split)
-        split_dir = pathlib.Path(split_dir)
+    with tempfile.TemporaryDirectory() as output_dir:
+        ROOT = pathlib.Path(__file__).resolve().parent
 
-        for name in tqdm(os.listdir(split_dir)):
-            path = os.path.join(split_dir, name)
+        for dataset in tqdm(datasets, desc="Pre-processing"):
+            modname = dataset
+            modpath = str(ROOT / f"{dataset}.py")
 
-            basename, skew = name.split("[")
+            pydataset = srcfile_import(modpath, modname)
+            pydataset.preprocess(output_dir)
 
-            skew, exte = skew.split("]")
+        for dataset in tqdm(datasets, desc="Augmentation"):
+            dataset_dir = os.path.join(output_dir, dataset)
 
-            skew = float(skew)
-            exte = exte.replace(".", "")
+            metadata = os.path.join(dataset_dir, "metadata.csv")
+            metadata = pd.read_csv(metadata)
 
-            out_name = f"{basename}.{exte}"
-            out_path = os.path.join(images_dir, out_name)
+            metadata = metadata.sample(frac=1.0)
 
-            if out_name in examples:
-                continue
+            splits = []
 
-            image = Image.open(path)
+            for split in ["train", "valid", "test"]:
+                fullvar = f"{split}_split"
+                ratio = locals()[fullvar]
 
-            array = np.asarray(image)
-            array = rotate(array, -skew, cval=1.0)
+                num_split = int(len(metadata) * ratio)
 
-            array = array * 255
-            array = array.astype(np.uint8)
+                splits += [split] * num_split
 
-            image = Image.fromarray(array)
-            image.save(out_path)
+            metadata["split"] = splits
 
-            examples.append(out_name)
+            for series in metadata.itertuples():
+                image_name, image_exte = series.image.rspit(".", 1)
 
-    random.seed(seed)
-    random.shuffle(examples)
+                image = Image.open(series.imagepath)
 
-    num_examples = len(examples)
+                array = np.asarray(image)
+                array = rotate(array, -series.angle, cval=1.0)
 
-    splits = []
+                array = array * 255
+                array = array.astype(np.uint8)
 
-    for split in ["train", "valid", "test"]:
-        fullvar = f"{split}_split"
-        ratio = locals()[fullvar]
+                out_image_name = f"{image_name}.{image_exte}"
+                out_image_path = os.path.join(images_dir, out_image_name)
 
-        num_split = int(num_examples * ratio)
+                image = Image.fromarray(array)
+                image.save(out_image_path)
 
-        splits += [split] * num_split
+                input = (out_image_name, series.split, dataset)
+                examples.append(input)
+
+                transform = np.random.choice(
+                    angle_transform, num_angle_transforms, replace=False
+                )
+
+                for idx, angle in enumerate(transform):
+                    rotated = rotate(array, angle, cval=1.0)
+
+                    rotated = rotated * 255
+                    rotated = rotated.astype(np.uint8)
+
+                    key = f"{idx:03d}_000"
+
+                    out_image_name = f"{image_name}_{key}.{image_exte}"
+                    out_image_path = os.path.join(images_dir, out_image_name)
+
+                    rotated_image = Image.fromarray(rotated)
+                    rotated_image.save(out_image_path)
+
+                    input = (out_image_name, series.split, dataset)
+                    examples.append(input)
+
+                    for jdx in range(num_image_transforms):
+                        if jdx == 0:
+                            continue
+
+                        transformed = image_transform(image=rotated)["image"]
+
+                        transformed = transformed * 255
+                        transformed = transformed.astype(np.uint8)
+
+                        key = f"{idx:03d}_{jdx:03d}"
+
+                        out_image_name = f"{image_name}_{key}.{image_exte}"
+                        out_image_path = os.path.join(images_dir, out_image_name)
+
+                        transformed_image = Image.fromarray(transformed)
+                        transformed_image.save(out_image_path)
+
+                        input = (out_image_name, series.split, dataset)
+                        examples.append(input)
 
     dataframe = pd.DataFrame.from_records(
-        zip(examples, splits), columns=["filename", "split"]
+        examples, columns=["filename", "split", "dataset"]
     )
 
     for split in ["train", "valid", "test"]:
@@ -141,10 +300,30 @@ def main(
     if artifact is not None:
         env = wandb.init()
 
-        artifact = wandb.Artifact(artifact, type="dataset")
+        if image_transform is not None:
+            image_transform = albumentations.to_dict(image_transform)
 
-        artifact.add_dir(images_dir)
-        artifact.add_dir(annotations_dir)
+        artifact_kwargs = {
+            "description": "A document image skew estimation collection",
+            "metadata": {
+                "datasets": tuple(datasets),
+                "stop-angle": stop_angle,
+                "step-angle": step_angle,
+                "image-transform": image_transform,
+                "num-image-transforms": num_image_transforms,
+                "num-angle-transforms": num_angle_transforms,
+                "train-split": train_split,
+                "valid-split": valid_split,
+                "test-split": test_split,
+            },
+            "project": "neural-deskew",
+            "type": "dataset",
+        }
+
+        artifact = wandb.Artifact(artifact, **artifact_kwargs)
+
+        artifact.add_dir(images_dir, "images")
+        artifact.add_dir(annotations_dir, "annotations")
 
         env.log_artifact(artifact)
 
